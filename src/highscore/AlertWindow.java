1f6ac4e1561997561bdcf3cb499a5ad5e5ddeffb
Farhad <fa@neodesign.se>
2015-10-20 13:28:23 +0200

    RaiseFlag

---
+++ src/highscore/AlertWindow.java
@@ -0,0 +1,48 @@
+package highscore;
+
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+/**
+ * Created by Farhad Atroshi on 06/10/15.
+ * This class displays an AlertWindow with customizable title and message body
+ * The method displayBox is static and therefore can be called directly from this class.
+ * Inspiration from youtube tutorial: https://youtu.be/SpL3EToqaXA?list=PL6gx4Cwl9DGBzfXLWLSYVy8EbTdpGbUIG
+ */
+public class AlertWindow {
+    public static void show(String windowTitle, String msg, double width, double height) {
+        Stage w = new Stage();
+        // Set title
+        w.setTitle(windowTitle);
+        // Set window dim.
+        w.setMinWidth(width);
+        w.setMinHeight(height);
+        //
+        w.initModality(Modality.APPLICATION_MODAL);
+        // Window body text
+        Label bodyText = new Label();
+        bodyText.setFont(Font.font(17));
+        bodyText.setStyle("-fx-text-alignment: center");
+        bodyText.setText(msg);
+        // Close btn
+        Button btn = new Button("Close");
+        btn.setOnAction(event -> w.close());
+
+        //Create layout
+        VBox layout = new VBox(50);
+        layout.setStyle("-fx-background-color: #8fbc8f;");
+        layout.getChildren().addAll(bodyText, btn);
+
+        // Create scene
+        Scene scene = new Scene(layout);
+        w.setScene(scene);
+        w.initStyle(StageStyle.TRANSPARENT);
+        w.showAndWait();
+    }
+}
---
+++ src/highscore/HighScore.java
@@ -0,0 +1,40 @@
+package highscore;
+
+import java.io.Serializable;
+
+/**
+ * Created by Farhad Atroshi on 07/10/15.
+ * 
+ */
+public class HighScore implements Serializable {
+    private String username;
+    private String hand;
+    private int pot;
+    
+    public HighScore (String username,String hand,double pot){
+        this.username = username;
+        this.hand = hand;
+        this.pot = (int)pot;
+    }
+    
+    public String getUserName(){
+        return this.username;
+    }
+    
+    public String getHand(){
+        return this.hand;
+        
+    }
+    
+    public int getPot(){
+        return this.pot;
+    }
+    
+   
+    
+    @Override
+    public String toString(){
+        String info = (" " + this.username + " " + this.hand + " " + this.pot);
+        return info;
+    }
+}

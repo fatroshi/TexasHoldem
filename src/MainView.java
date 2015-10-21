6552be519089ed34ca0be3db68e7bebf9e1698c6
Farhad <fa@neodesign.se>
2015-10-22 00:33:11 +0200

    added comments

--- src/MainView.java
+++ src/MainView.java
@@ -18,10 +18,6 @@ public class MainView extends Application{
 
     Stage window;
 
-   // HighScoreList hsl = new HighScoreList();
-   // DB db = new DB("dbScoreList.bin");
-    // testing
-
     public static void main(String[] args) {
         // Set program as a javaFx application
         // Then calls start(Stage primaryStage)
@@ -31,8 +27,6 @@ public class MainView extends Application{
 
     @Override
     public void start(Stage stage) {
-
-
         // Root
         BorderPane root     = new BorderPane();
 
@@ -42,38 +36,6 @@ public class MainView extends Application{
         // Game controller
         Controller controller = new Controller(viewStart);
 
-        /*
-        // MENU BAR
-        MenuBar mb          = new MenuBar();
-        VBox topVBox        = new VBox();
-        VBox centerBox      = new VBox();
-
-        Menu fileMenu       = new Menu("File");
-        MenuItem openItem   = new MenuItem("Open");
-        MenuItem closeItem  = new MenuItem("Close");
-        MenuItem highscoreItem  = new MenuItem("High Score");
-        MenuItem exitItem   = new MenuItem("Exit");
-
-        // END
-        fileMenu.getItems().addAll(openItem, closeItem,highscoreItem, exitItem);
-        
-        // shows highscorelist
-        highscoreItem.setOnAction(event -> highscore.AlertWindow.show("High Score List",db.getData().toString(), 3000, 3000));
-        //
-        mb.getMenus().addAll(fileMenu);
-        topVBox.getChildren().add(mb);
-        //paneRoot.getChildren().add(topVBox);
-        // END MENU BAR
-
-        // TableLogic
-        GameBackground start = new GameBackground(GameBackground_.TABLE_BLACK.getImageSrc());
-
-        */
-
-        // Add table to scene 
-        //root.getChildren().add(start.getImageView());
-
-        //
         window = stage;
         // Set title for the stage
         window.setTitle("KING KONG POKER");

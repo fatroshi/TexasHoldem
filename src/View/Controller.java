74d716e18a1ca9448679a07d66d9428c83db231a
Farhad <fa@neodesign.se>
2015-10-23 14:13:46 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/Layout/ChipLayout.java

--- src/View/Controller.java
+++ src/View/Controller.java
@@ -65,8 +65,8 @@ public class Controller {
      * creates players for the game
      */
     public void createPlayers() {
-        game.addPlayer("Mr Cohen", 50);
-        game.addPlayer("Mr Atroshi", 82);
+        game.addPlayer("Mr Cohen", 2979);
+        game.addPlayer("Mr Atroshi", 2979);
         //game.addPlayer("Felicia", 13);
         //game.addPlayer("Elise", 12);
     }
